c0faf2bcd566227c2d4d7f763fff126ba4451302
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-11-08 23:04:23 -0400

    Initial Commit

---
+++ ToDoIsh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoIsh
+//
+//  Created by Sofia Rodriguez Morales on 11/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoIsh/ToDoIshApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoIshApp.swift
+//  ToDoIsh
+//
+//  Created by Sofia Rodriguez Morales on 11/8/20.
+//
+
+import SwiftUI
+
+@main
+struct ToDoIshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
